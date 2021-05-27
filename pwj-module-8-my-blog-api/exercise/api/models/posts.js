3964e63a64a0314a603c800afb3b0af5d32909d4
Phil Laub <PHILLIP.LAUB.EVO@GMAIL.COM>
2021-05-27 09:42:36 -0700

    api get requests working, tested with postman

--- pwj-module-8-my-blog-api/exercise/api/models/posts.js
+++ pwj-module-8-my-blog-api/exercise/api/models/posts.js
@@ -7,18 +7,29 @@ class Post {
     return this.readData();
   }
 
-  getIndividualBlog() {
-    //get one blog post
+  getIndividualBlog(postId) {
+    //get one blog post\
+    const posts = this.readData();
+    const foundPost = posts.find((post) => post.id == postId);
+    return foundPost;
   }
 
-  add() {
+  add(newPost) {
     //add new post
+    const currentPosts = this.readData();
+    currentPosts.unshift(newPost);
+    this.storeData(currentPosts);
   }
 
   readData() {
     let rawData = fs.readFileSync(PATH);
     let posts = JSON.parse(rawData);
     return posts;
+  }
+
+  storeData(rawData) {
+    let data = JSON.stringify(rawData);
+    fs.writeFileSync(PATH, data);
   }
 }
 
