1aa4297ef1a9f8bc280bf818b1ad496bdebe8307
Phil Laub <PHILLIP.LAUB.EVO@GMAIL.COM>
2021-05-28 08:59:41 -0700

    post requests functional

--- pwj-module-8-my-blog-api/exercise/app.js
+++ pwj-module-8-my-blog-api/exercise/app.js
@@ -4,6 +4,8 @@ const Post = require("./api/models/posts");
 
 const postsData = new Post();
 
+app.use(express.json());
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   next();
@@ -23,6 +25,18 @@ app.get("/api/posts/:post_id", (req, res) => {
   } else {
     res.status(404).send("Not found!");
   }
+});
+
+app.post("/api/posts", (req, res) => {
+  const newPost = {
+    id: `${Date.now()}`,
+    title: req.body.title,
+    content: req.body.content,
+    post_image: req.body["post-image"],
+    added_date: `${Date.now()}`,
+  };
+  postsData.add(newPost);
+  res.status(201).send(newPost);
 });
 
 app.listen(3000, () => console.log("Listening on local host 3000"));
