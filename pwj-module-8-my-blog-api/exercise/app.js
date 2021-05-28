2d893b1c05f5a9858df7ca1ca2131aa2d2481103
Phil Laub <PHILLIP.LAUB.EVO@GMAIL.COM>
2021-05-28 09:28:25 -0700

    API requests complete

--- pwj-module-8-my-blog-api/exercise/app.js
+++ pwj-module-8-my-blog-api/exercise/app.js
@@ -1,42 +1,62 @@
 const express = require("express");
 const app = express();
+var multer = require("multer");
 const Post = require("./api/models/posts");
-
 const postsData = new Post();
 
-app.use(express.json());
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${file.fieldname}-${Date.now()}${getExt(file.mimetype)}`);
+  },
+});
 
-app.use((req, res, next) => {
+const getExt = (mimetype) => {
+  switch (mimetype) {
+    case "image/png":
+      return ".png";
+    case "image/jpeg":
+      return ".jpg";
+  }
+};
+
+var upload = multer({ storage: storage });
+
+app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   next();
 });
-
+app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
 app.get("/api/posts", (req, res) => {
   res.status(200).send(postsData.get());
 });
 
-app.get("/api/posts/:post_id", (req, res) => {
-  const postId = req.params.post_id;
-  const foundPost = postsData.getIndividualBlog(postId);
+app.get("/api/posts/:postId", (req, res) => {
+  const postId = req.params.postId;
+  const posts = postsData.get();
+  const foundPost = posts.find((post) => post.id == postId);
   if (foundPost) {
     res.status(200).send(foundPost);
   } else {
-    res.status(404).send("Not found!");
+    res.status(404).send("Not Found");
   }
 });
 
-app.post("/api/posts", (req, res) => {
+app.post("/api/posts", upload.single("post-image"), (req, res) => {
+  let filePath = req.file.path.replace("\\", "/");
   const newPost = {
     id: `${Date.now()}`,
     title: req.body.title,
     content: req.body.content,
-    post_image: req.body["post-image"],
+    post_image: filePath,
     added_date: `${Date.now()}`,
   };
   postsData.add(newPost);
   res.status(201).send(newPost);
 });
 
-app.listen(3000, () => console.log("Listening on local host 3000"));
+app.listen(3000, () => console.log("Listening on http://localhost:3000/"));
